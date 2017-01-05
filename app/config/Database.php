0bb2dbe4f0411690dae3aae4a7f466d12a511bae
Max Taylor <max@cakerstream.com>
2017-01-05 15:09:10 +0000

    Removing all files

--- app/config/Database.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('DB_TYPE', 'mysql');
-define('DB_NAME','migrations');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_HOST', '127.0.0.1');
--- app/config/keys.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('SITE_KEY', '5ysp96A1D1gVVpHb4cb06MHGNp7HO827');
